3ee758c949be8909ffc92f8584aefc2b796b256c
cabra <e2cabraljunior@gmail.com>
2025-12-10 20:12:31 -0300

    feat(container): bind AuthService and ParameterStorageService to the container
    Added bindings for `AuthService` and `ParameterStorageService` to the container to enable centralized dependency injection for authentication and parameter management services.

--- src/infra/container/index.ts
+++ src/infra/container/index.ts
@@ -8,12 +8,18 @@ import { IStorage } from '../interfaces/i-storage.interface';
 import { StorageService } from '../storage/storage.service';
 import { IMonitoringController } from '../../presentation/controllers/interfaces/i-monitoring.controller';
 import { MonitoringController } from '../../presentation/controllers/monitoring.controller';
+import { IParameterStorageService } from '../storage/interfaces/i-paramenter-storage.service';
+import { ParameterStorageService } from '../storage/parameter-storage.service';
+import { IAuthService } from '../auth/interfaces/i-auth.service';
+import { AuthService } from '../auth/auth.service';
 
 const container = new Container()
 
 container.bind<IDatabase>(TYPES.Infrastructure.Database).to(Database).inSingletonScope();
 container.bind<ILogger>(TYPES.Infrastructure.Logger).to(Logger).inSingletonScope();
 container.bind<IStorage>(TYPES.Infrastructure.Storage).to(StorageService).inSingletonScope();
+container.bind<IParameterStorageService>(TYPES.Infrastructure.ParameterStorage).to(ParameterStorageService).inSingletonScope();
+container.bind<IAuthService>(TYPES.Infrastructure.Auth).to(AuthService).inSingletonScope();
 
 container.bind<IMonitoringController>(TYPES.Controllers.MonitoringController).to(MonitoringController);
 
